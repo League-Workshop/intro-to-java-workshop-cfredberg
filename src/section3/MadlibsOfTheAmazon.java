fb6c9bfadce2b8574931857bab5edac44e4f97ee
League Student <league@iMac-5.attlocal.net>
2019-08-14 14:26:43 -0700

    day 3

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,20 +7,22 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
+		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it...");
 		// Get the user to enter an adjective
-
+		String adj = JOptionPane.showInputDialog("Please enter an adjective.");
 		// Get the user to enter a type of liquid
-
+		String liquid = JOptionPane.showInputDialog("Please enter a type of liquid.");
 		// Get the user to enter a body part
-
+		String body = JOptionPane.showInputDialog("Please enter a body part.");
 		// Get the user to enter a verb
-
+		String verb = JOptionPane.showInputDialog("Please enter a verb.");
 		// Get the user to enter a place
-
+		String place = JOptionPane.showInputDialog("Please enter a place.");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
+		String BFF = JOptionPane.showInputDialog("Piranhas are more " + adj + " during the day, so cross the river at night.");
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
+		JOptionPane.showMessageDialog("Piranhas are attracted to fresh " + liquid + " and will most likely take a bite out of your " + body + " if you " + verb + ". Whatever you do, if you have an open wound, try to find another way to get back to the ")
 		// likely take a bite out of your [body part] if you [verb]. Whatever
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!

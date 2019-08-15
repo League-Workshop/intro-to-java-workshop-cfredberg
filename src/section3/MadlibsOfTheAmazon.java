0b32f43a6b72e191893b008a0de88a5ce2d0229d
cfredberg <53453343+cfredberg@users.noreply.github.com>
2019-08-14 18:36:16 -0700

    add ons

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,7 +7,6 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it...");
 		// Get the user to enter an adjective
 		String adj = JOptionPane.showInputDialog("Please enter an adjective.");
 		// Get the user to enter a type of liquid
@@ -20,15 +19,13 @@ public class MadlibsOfTheAmazon {
 		String place = JOptionPane.showInputDialog("Please enter a place.");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
-		String BFF = JOptionPane.showInputDialog("Piranhas are more " + adj + " during the day, so cross the river at night.");
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
-		JOptionPane.showMessageDialog("Piranhas are attracted to fresh " + liquid + " and will most likely take a bite out of your " + body + " if you " + verb + ". Whatever you do, if you have an open wound, try to find another way to get back to the ")
 		// likely take a bite out of your [body part] if you [verb]. Whatever
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
 		
 		// Make a pop-up for the final story. You can use \n to go to the next line
-		
+		JOptionPane.showMessageDialog(null, "Piranhas are more " + adj + " during the day, \n so cross the river at night. \n Piranhas are attracted to fresh " + liquid + " \n and will most likely take a bite out of your " + body + " if you " + verb + ". \n Whatever you do, if you have an open wound, \n try to find another way to get back to the " + place + ". \n Good luck!");
 
 	}
 }
